5626b018b54f2063664e3f272167358fb05fe745
Godfrey Owidi <godfreyowiidi@gmail.com>
2022-01-11 21:28:55 +0300

    listening to the sockets.

---
+++ networking/Sockets/weserver-sockets.hpp
@@ -0,0 +1,14 @@
+#ifndef webserver_sockets_hpp
+#define webserver_sockets_hpp
+
+#include <stdio.h>
+
+#include "WebSocket.hpp"
+
+#include "BindingSocket.hpp"
+#include "ListeningSocket.hpp"
+
+#include "ConnectingSocket.hpp"
+
+#endif /* webserver_sockets_hpp */
+
---
+++ networking/webserver-networking.hpp
@@ -0,0 +1,9 @@
+#ifndef webserver_networking_hpp
+#define webserver_networking_hpp
+
+#include <stdio.h>
+
+#include "Sockets/weserver-sockets.hpp"
+
+
+#endif /* webserver_networking_hpp */
